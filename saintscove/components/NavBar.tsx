9876a525e17d29df980759cb4623bdf6b2c41a1b
agathaa <aamoako1120@gmail.com>
2024-02-19 14:22:32 -0500

    updated navbar

--- saintscove/components/NavBar.tsx
+++ saintscove/components/NavBar.tsx
@@ -4,14 +4,16 @@ import Logo from './Logo'
 
 const Navbar = () => {
   return (
-    <nav className='flex space-x-6 border-b'>
-        <Link href="/"><a><Logo /></a></Link>
-        <ul className='flex space-x-6'>
-            <li><Link href="/"><a>About/Missions</a></Link></li>
-            <li><Link href="/connect"><a>Connect</a></Link></li>
-            <li><Link href="/media"><a>Media</a></Link></li>
-            <li><Link href="/team"><a>Meet The Team</a></Link></li>
-        </ul>
+    <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
+        <div className="flex items-center flex-shrink-0 text-white mr-10"><Logo /></div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+            <div className="text-sm lg:flex-grow">
+                <Link href="/about" className="block mt-4 lg:inline-block lg:mt-o text-white hover:text-white mr-4">About/Missions</Link>
+                <Link href="/connect" className="block mt-4 lg:inline-block lg:mt-o text-white hover:text-white mr-4">Connect</Link>
+                <Link href="/media" className="block mt-4 lg:inline-block lg:mt-o text-white hover:text-white mr-4">Media</Link>
+                <Link href="/team" className="block mt-4 lg:inline-block lg:mt-o text-white hover:text-white mr-4">Meet The Team</Link>
+            </div>
+        </div>
     </nav>
   )
 }
