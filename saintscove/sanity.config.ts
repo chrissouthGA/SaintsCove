8c418ef47daa6d7029714023c3bd066c6d322fa6
agathaa <aamoako1120@gmail.com>
2024-02-21 23:08:59 -0500

    created schema

--- saintscove/sanity.config.ts
+++ saintscove/sanity.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "sanity";
 import { deskTool } from "sanity/desk";
+import blog from "./sanity/schemas/blog-schema";
+
+
+
 
 const config = defineConfig({
     projectId: 'bqz1l84d',
@@ -7,7 +11,8 @@ const config = defineConfig({
     title: 'SaintsCove Blog',
     apiVersion: '2024-21-02',
     basePath: '/admin',
-    plugins: [deskTool()]
+    plugins: [deskTool()], 
+    schema: {types:[ blog ]},   
 })
 
 export default config;
