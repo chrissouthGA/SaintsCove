04a5bdcea739ef2715bbc6cd7fd9d68400a99349
agathaa <aamoako1120@gmail.com>
2024-02-29 16:55:09 -0500

    updated meet the team page

--- saintscove/components/Description.tsx
+++ saintscove/components/Description.tsx
@@ -1,20 +1,39 @@
-import React from 'react'
-import { images } from '../utils/constants'
+import React from 'react';
+import { images } from '../utils/constants';
+import left from '../public/left.svg';
+import right from '../public/right.svg'
+import Image from 'next/image';
 
-const Description = () => {
-  return (
-    <div className='grid place-items-start w-full bg-[#916d51] relative rounded-tr-3xl rounded-br-3xl'>
-        <div className='uppercase text-sm absolute right-4 top-2 underline-offset-4 underline'>
-            Meet The Team
-            {images.map((elem, idx) => (
-                <div className='block w-full h-[80vh] object-cover transition-all duration-500 ease-in-out' key={idx}>
-                    <div className='py-16 text-5xl font-extrabold'>{elem.title}</div>
-                    <div className='leading-relaxed font-medium text-base tracking-wide h-40 italic text-gray-600'>{" "}{elem.desc}</div>
-                </div>
-            ))}
+type Props = {
+    clickNext:any, 
+    clickPrev:any,
+    activeImgIndex:any
+}
+
+const Description = ({clickNext, clickPrev, activeImgIndex}:Props) => {
+    return (
+        <div className='grid place-items-start w-full bg-[#916d51] relative rounded-tr-3xl rounded-br-3xl'>
+            <div className='uppercase text-sm absolute right-4 top-2 underline-offset-4 underline'>
+                Meet The Team
+                {images.map((elem, idx) => (
+                    <div className={idx === activeImgIndex ? 'block w-full h-[80vh] object-cover transition-all duration-500 ease-in-out' : 'hidden'} key={idx}>
+                        <div className='py-16 text-5xl font-extrabold'>{elem.title}
+                        </div>
+                        <div className='leading-relaxed font-medium text-base tracking-wide h-40 italic text-gray-600'>{" "}{elem.desc}
+                        </div>
+                        <div className='absolute bottom-1 w-full flex justify-center items-center'>
+                            <div onClick={clickPrev} className='absolute bottom-2 right-10 cursor-pointer'>
+                                <Image src={left} alt="" width={30} height={30} />
+                            </div>
+                            <div onClick={clickNext} className='absolute bottom-2 right-2 cursor-pointer'>
+                                <Image src={right} alt="" width={30} height={30} />
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Description
--- saintscove/components/Slider.tsx
+++ saintscove/components/Slider.tsx
@@ -1,25 +1,54 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect, useState } from 'react'
 import { images } from '../utils/constants'
 import Image from 'next/image'
 import Description from './Description'
 
 
 const Slider = () => {
+    const [activeImage, setActiveImage] = useState(0)
+
+    const clickNext = () => {
+        activeImage === images.length - 1 
+        ? setActiveImage(0)
+        : setActiveImage(activeImage + 1)
+    }; 
+
+    const clickPrev = () => {
+        activeImage === 0 
+        ? setActiveImage(images.length - 1)
+        : setActiveImage(activeImage - 1)
+    };
+
+    useEffect(() => {
+        const timer = setTimeout (() => {
+            clickNext()
+        }, 5000);
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [activeImage])
+
   return (
     <div className='grid place-items-center grid-cols-2 w-full mx-auto max-w-5xl shadow-2xl rounded-2xl'>
         <div className='w-full flex justify-center items-center gap-4 transition-transform ease-in-out duration-500 rounded-2xl'>
             {images.map((pic,idx) =>(
-                <div className='block w-full h-[80vh] object-cover transition-all duration-500 ease-in-out' key={idx}>
+                <div className={`${idx === activeImage ? 'block w-full h-[80vh] object-cover transition-all duration-500 ease-in-out': 'hidden'}`} key={idx}>
                     <Image 
                     src={pic.src}
                     alt=""
-                    width={350}
-                    height={350}
+                    width={300}
+                    height={200}
                     className="w-full h-full object-cover rounded-t-3xl rounded-b-3xl" 
                     />
                 </div>
             ))}
-            <Description />
+            <Description 
+                activeImgIndex={activeImage}
+                clickNext={clickNext}
+                clickPrev={clickPrev}
+            />
         </div>
     </div>
   )
