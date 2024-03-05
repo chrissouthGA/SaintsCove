0bd1f26bc2eedf78b49c4c9dee2202d76bbe5d5b
agathaa <aamoako1120@gmail.com>
2024-03-04 21:22:45 -0500

    updated connect page

--- saintscove/src/app/connect/page.tsx
+++ saintscove/src/app/connect/page.tsx
@@ -7,9 +7,7 @@ import { Blog } from "../../../types/Blogs";
 import Image from "next/image";
 import Link from "next/link";
 
-interface Props {
-  blogs?: Blog[];
-}
+
 
 const ConnectPage = ({ }: Props) => {
   const [blogs, setBlogs] = useState<Blog[]>([]);
